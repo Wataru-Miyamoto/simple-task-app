80fa29462028a677ec25d8d137b7d7e697593245
EC2 Default User <ec2-user@ip-172-31-37-13.ap-northeast-1.compute.internal>
2019-11-25 12:08:52 +0000

    Finish task-system

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_user
-  before_action :set_task, only %i(show edit update destroy)
+  before_action :set_task, only: %i(show edit update destroy)
   before_action :logged_in_user
   before_action :correct_user
   
@@ -16,7 +16,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = @user.tasks.build(task.params)
+    @task = @user.tasks.build(task_params)
     if @task.save
       flash[:success] = "タスクを新規作成しました。"
       redirect_to user_tasks_url @user
@@ -31,7 +31,7 @@ class TasksController < ApplicationController
   def update
     if @task.update_attributes(task_params)
       flash[:success] = "タスクを更新しました。"
-      redirect_to user_tasks_url(@user, @task)
+      redirect_to user_task_url(@user, @task)
     else
       render :edit
     end
